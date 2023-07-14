45cea5026cb2778ad9e0a36ff10cd70921c3e03d
Victor Hugo Cornejo Calderon <rex2002xp@gmail.com>
2023-07-14 08:37:50 -0600

    Se actualiza para calcular el ancho y alto de la ventana.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
 const bootstrap = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        autoHideMenuBar: true,
-    })
-    win.loadURL('https://chat.openai.com/')
-}
+  const { width, height } = getBounds();
+
+  const win = new BrowserWindow({
+    width,
+    height,
+  });
+  win.loadURL("https://chat.openai.com/");
+};
+
+const getBounds = () => {
+  // check if the window is larger than the screen
+  const { width: screenWidth, height: screenHeight } =
+    require("electron").screen.getPrimaryDisplay().workAreaSize;
+
+  // 80% of the screen
+  const width = Math.round(screenWidth * 0.8);
+  const height = Math.round(screenHeight * 0.8);
+
+  return { width, height };
+};
+
 
 app.whenReady().then(bootstrap);
